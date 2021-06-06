9f43833b89e37584ebb31faa128a9076adf891a6
Josue Daniel Palma <jod_palmam@unicah.edu>
2021-06-06 16:08:23 -0600

    ya esta

--- src/main/java/com/programulti/minecraft/conn.java
+++ src/main/java/com/programulti/minecraft/conn.java
@@ -66,6 +66,90 @@ public class conn {
             return null;
         }
     }
+     
+     public void eliminarRegistro(MinecraftEntry registroAEliminar){
+        try {
+            String sentenciaSQL = "DELETE from MINECRAFT where ID=%d;";
+            Statement comandoSQL = c.createStatement();
+            comandoSQL.executeUpdate(
+                    String.format(
+                            sentenciaSQL,
+                            registroAEliminar.getID()
+                    )
+            );
+            comandoSQL.close();
+        } catch(Exception ex){
+            System.err.println(" Error " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+     
+     public void actualizarRegistro(MinecraftEntry registroAActualizar){
+        try {
+            String sentencialSQL = "UPDATE MINECRAFT set MINECRAFTBLOCKNAME='%s', "
+                    + "MINECRAFTCRAFTRECEIPT='%s', MINECRAFTATTACK='%d', MINECRAFTDEFENCE='%d' "
+                    + "where ID=%d;";
+            Statement comandoSQL = c.createStatement();
+            comandoSQL.executeUpdate(
+                    String.format( 
+                            sentencialSQL,
+                            registroAActualizar.getMINECRAFTBLOCKNAME(),
+                            registroAActualizar.getMINECRAFTCRAFTRECEIPT(),
+                            registroAActualizar.getMINECRAFTATTACK(),
+                            registroAActualizar.getMINECRAFTDEFENCE(),
+                            registroAActualizar.getID()
+                    )
+            );
+            comandoSQL.close();
+        } catch (Exception ex){
+            System.err.println(" Error " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+     
+     
+     public void addNewMinecraftEntry(MinecraftEntry newEntry){
+        try {
+        String sentenciaSql = "INSERT INTO MINECRAFT (MINECRAFTBLOCKNAME,MINECRAFTCRAFTRECEIPT,"
+                + "MINECRAFTATTACK,MINECRAFTDEFENCE) valueS ('%s','%s','%d','%d');";
+        Statement comandoSql = c.createStatement();
+        comandoSql.executeUpdate(String.format(sentenciaSql,
+                newEntry.getMINECRAFTBLOCKNAME(),
+                newEntry.getMINECRAFTCRAFTRECEIPT(),
+                newEntry.getMINECRAFTATTACK(),
+                newEntry.getMINECRAFTDEFENCE()
+            )
+        );
+        comandoSql.close();
+        } catch (Exception e) {
+            System.err.println(" Error " + e.getMessage());
+            System.exit(0);
+        }
+    }
+     
+     public MinecraftEntry obtenerUnRegistro( int idRegistro) {
+        try{
+            String setenciaSql = "SELECT * from MINECRAFT where ID=%d;";
+            Statement comandoSql = c.createStatement();
+            ResultSet cursorDeRegistro = comandoSql.executeQuery(
+                String.format(setenciaSql, idRegistro)
+            );
+            MinecraftEntry miRegistro = new MinecraftEntry();
+            while ( cursorDeRegistro.next()){
+                miRegistro.setID(cursorDeRegistro.getInt("ID"));
+                miRegistro.setMINECRAFTBLOCKNAME(cursorDeRegistro.getString("MINECRAFTBLOCKNAME"));
+                miRegistro.setMINECRAFTCRAFTRECEIPT(cursorDeRegistro.getString("MINECRAFTCRAFTRECEIPT"));
+                miRegistro.setMINECRAFTATTACK(cursorDeRegistro.getInt("MINECRAFTATTACK"));
+                miRegistro.setMINECRAFTDEFENCE(cursorDeRegistro.getInt("MINECRAFTDEFENCE"));
+            }
+            comandoSql.close();
+            return miRegistro;
+        } catch (Exception e) {
+            System.err.println(" Error " + e.getMessage());
+            System.exit(0);
+            return null;
+        }
+    }
     
     
 }
--- src/main/java/com/programulti/minecraft/main.java
+++ src/main/java/com/programulti/minecraft/main.java
@@ -63,31 +63,31 @@ public class main {
         System.out.println(LineSeperator);
         
               
-        while (!inputData.equalsIgnoreCase("Q")) {
+        while (!inputData.equalsIgnoreCase("S")) {
             System.out.println();
             switch (inputData.toUpperCase()) {
-                case "R": 
-                    
-                    break;
                 case "N": 
-                   
+                   nuevoobjeto();
                     break;
-                case "U": 
+                case "A": 
+                   actualizarRegistro();
+                    break;
+                case "E": 
+                     eliminarRegistro();
+                    break;
                     
-                    break;
-                case "D": 
                     
-                    break;
             }
             mostrar();
-            menu();
+            menu();           
         } 
+        System.out.println("Programa finaliazado");
     }
     
     
     public static void menu(){
         System.out.println(LineSeperator);
-        System.out.println("Q Quit\tR Reload\tN New\tU Update\tD Delete \n Press Key and Enter: ");
+        System.out.println("\tN Nuevo \tA Actualizar\tE Eliminar \tS Salir\n Presione una tecla ");
         inputData = keyInput.nextLine();
     }
     
@@ -106,4 +106,108 @@ public class main {
         System.out.println(String.format("Total Rows: %d",myEntries.size()));
 }
 
+    
+    
+    public static void nuevoobjeto(){
+        System.out.println();
+        System.out.println("Getting New Data");
+        System.out.println(LineSeperator);
+        System.out.println("MINECRAFTBLOCKNAME: ");
+        String MINECRAFTBLOCKNAME = keyInput.nextLine();
+        System.out.println("MINECRAFTCRAFTRECEIPT: ");
+        String MINECRAFTCRAFTRECEIPT = keyInput.nextLine();
+        System.out.println("MINECRAFTATTACK: ");
+        int MINECRAFTATTACK = keyInput.nextInt();
+        System.out.println("MINECRAFTDEFENCE: ");
+        int MINECRAFTDEFENCE = keyInput.nextInt();
+        MinecraftEntry newEntry = new MinecraftEntry();
+        newEntry.setMINECRAFTBLOCKNAME(MINECRAFTBLOCKNAME);
+        newEntry.setMINECRAFTCRAFTRECEIPT(MINECRAFTCRAFTRECEIPT);
+        newEntry.setMINECRAFTATTACK(MINECRAFTATTACK);
+        newEntry.setMINECRAFTDEFENCE(MINECRAFTDEFENCE);
+        
+        myConnection.addNewMinecraftEntry(newEntry);
+        
+        System.out.println();
+        
+    }
+    
+    public static void actualizarRegistro(){
+        
+        System.out.println("Escriba el código del registro a actualizar:");
+        int idRegistro = keyInput.nextInt();
+      
+        keyInput.nextLine();
+
+        MinecraftEntry registroAActualizar = myConnection.obtenerUnRegistro(idRegistro);
+        
+        
+        if (registroAActualizar.getID() > 0){
+            System.out.println(LineSeperator);
+            System.out.println(registroAActualizar.getFormattedText());
+            System.out.println(LineSeperator);
+           
+            System.out.println("MINECRAFTBLOCKNAME (" + registroAActualizar.getMINECRAFTBLOCKNAME() + "):");
+            String MINECRAFTBLOCKNAME = keyInput.nextLine();
+
+            if (!MINECRAFTBLOCKNAME.isEmpty() && !MINECRAFTBLOCKNAME.equals(registroAActualizar.getMINECRAFTBLOCKNAME())){
+                registroAActualizar.setMINECRAFTBLOCKNAME(MINECRAFTBLOCKNAME);
+            }
+            
+            System.out.println("MINECRAFTCRAFTRECEIPT (" + registroAActualizar.getMINECRAFTCRAFTRECEIPT()+ "):");
+            String MINECRAFTCRAFTRECEIPT = keyInput.nextLine();
+            if (!MINECRAFTCRAFTRECEIPT.isEmpty() && !MINECRAFTCRAFTRECEIPT.equals(registroAActualizar.getMINECRAFTCRAFTRECEIPT())){
+                registroAActualizar.setMINECRAFTCRAFTRECEIPT(MINECRAFTCRAFTRECEIPT);
+            }
+            
+            
+            System.out.println("MINECRAFTATTACK (" + registroAActualizar.getMINECRAFTATTACK() + "):");
+            int MINECRAFTATTACK = keyInput.nextInt();
+            if (MINECRAFTATTACK!=registroAActualizar.getMINECRAFTATTACK()){
+                registroAActualizar.setMINECRAFTATTACK(MINECRAFTATTACK);
+            }
+            
+            
+            System.out.println("MINECRAFTDEFENCE (" + registroAActualizar.getMINECRAFTDEFENCE() + "):");
+            int MINECRAFTDEFENCE = keyInput.nextInt();
+            if (MINECRAFTDEFENCE!=registroAActualizar.getMINECRAFTDEFENCE()){
+                registroAActualizar.setMINECRAFTDEFENCE(MINECRAFTDEFENCE);
+            }
+            
+            System.out.println(registroAActualizar.getFormattedText());
+            myConnection.actualizarRegistro(registroAActualizar);
+            System.out.println("Registro Actualizado!!! Presione Enter para Continuar.");
+        } else {
+            System.out.println("No existe Registro !!! Presione Enter para Continuar.");
+        }
+        
+        keyInput.nextLine();
+    }
+    
+    public static void eliminarRegistro(){
+        System.out.println("Escriba el código del registro a eliminar:");
+        int idRegistro = keyInput.nextInt();
+
+        keyInput.nextLine();
+
+        MinecraftEntry registroAEliminar = myConnection.obtenerUnRegistro(idRegistro);
+        if (registroAEliminar.getID() > 0) {
+            System.out.println(LineSeperator);
+            System.out.println(registroAEliminar.getFormattedText());
+            System.out.println(LineSeperator);
+
+            System.out.println("¿Desea Eliminar el registro? (S/N):");
+            String opcion = keyInput.nextLine();
+            if (opcion.toUpperCase().equals("S")){
+                myConnection.eliminarRegistro(registroAEliminar);
+                System.out.println("Registro Eliminado. Presione Enter para continuar.");
+            } else {
+                System.out.println("Operación cancelada. Presione Enter para continuar.");
+            }
+        } else {
+            System.out.println("No existe Registro !!! Presione Enter para Continuar.");
+        }
+        
+        keyInput.nextLine();
+    }
 }
